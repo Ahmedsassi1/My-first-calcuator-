87fccc07a9473ae84725610da0b16c288443e7bd
Ahmedsassi1 <ahmed_sassi92@hotmail.com>
2022-07-11 18:20:06 +0200

    calculator

--- projet.n1.calculatrice/calculator-script.js
+++ projet.n1.calculatrice/calculator-script.js
@@ -1,1 +1,22 @@
-$('#head').css('color','red')
+$("#title").css("color","white")
+   .css("font-size","50px")
+   .css("margin-left","100px")
+   .css("font-family","Arial")
+$("body")
+   .css("background-image","url(" + "https://cdn.wallpapersafari.com/77/68/36oMOg.jpg" + ")")
+$("label")
+   .css("color","white")
+$("button")
+   .css("display","inline-block")
+   .css("padding","6px 12px")
+   .css("font-size","14px")
+   .hover()
+$("#result").css("color","white")
+
+$("#plus").on("click", function add (){ 
+	var a = parseInt($("#a").val());
+	var b = parseInt($("#b").val());
+	$("#result").html(a+b);
+	});
+
+
