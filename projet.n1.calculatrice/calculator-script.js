c39bd7211f32b3afa70eba1f9dbc94fb927de4ba
Ahmedsassi1 <ahmed_sassi92@hotmail.com>
2022-07-12 01:53:23 +0200

    calculator

--- projet.n1.calculatrice/calculator-script.js
+++ projet.n1.calculatrice/calculator-script.js
@@ -1,6 +1,6 @@
 $("#title").css("color","white")
    .css("font-size","50px")
-   .css("margin-left","100px")
+   .css("margin-left","80px")
    .css("font-family","Arial")
 $("body")
    .css("background-image","url(" + "https://cdn.wallpapersafari.com/77/68/36oMOg.jpg" + ")")
@@ -9,14 +9,38 @@ $("label")
 $("button")
    .css("display","inline-block")
    .css("padding","6px 12px")
-   .css("font-size","14px")
+   .css("font-size","35px")
    .hover()
-$("#result").css("color","white")
 
-$("#plus").on("click", function add (){ 
+
+$("#plus").on("click", function add (){
 	var a = parseInt($("#a").val());
 	var b = parseInt($("#b").val());
-	$("#result").html(a+b);
+   if (typeof(a) === 'number'&& typeof(b) ==='number')
+	{$("#result").html(a+b);}
+   else {$("#result").html('choose a number please');}
 	});
 
+$("#minus").on("click", function substract (){
+   {$("#result").html(parseInt($("#a").val())-parseInt($("#b").val()));}
+   });
 
+$("#multiply").on("click", function multiplication (){
+   {$("#result").html(parseInt($("#a").val())*parseInt($("#b").val()));}
+   });
+
+$("#division").on("click", function divise (){
+   {$("#result").html(parseInt($("#a").val())/parseInt($("#b").val()));}
+   });
+
+$("#modulo").on("click", function rest (){
+   {$("#result").html(parseInt($("#a").val())%parseInt($("#b").val()));}
+   });
+
+$("#percent").on("click", function pourcentage (){
+   {$("#result").html(parseInt($("#a").val())/parseInt($("#b").val())*100);}
+   });
+
+$("#pow").on("click", function power (){
+   {$("#result").html(Math.pow(parseInt($("#a").val()),parseInt($("#b").val())));}
+   });
